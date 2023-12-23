5cd04c7f3c8b9165c6d9b2f11ad4bbed9509497a
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2023-12-23 22:56:46 +0300

    small fixes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,35 +1,58 @@
 import type { DocumentSnapshot } from 'firebase/firestore';
+import { auth } from '$lib/firebase';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 export const mapDocumentWithId = (doc: DocumentSnapshot) => ({
-	id: doc.id,
-	...doc.data()
+  id: doc.id,
+  ...doc.data()
 });
 
 const lazyLoadOptions = {
-	root: null,
-	rootMargin: '0px',
-	threshold: 0
+  root: null,
+  rootMargin: '0px',
+  threshold: 0
 };
 
 export const lazyLoad = (image: HTMLImageElement, src: string) => {
-	const loaded = () => {
-		image.style.opacity = '1';
-	};
-	const observer = new IntersectionObserver((entries) => {
-		if (entries[0].isIntersecting) {
-			image.src = src;
-			if (image.complete) {
-				loaded();
-			} else {
-				image.addEventListener('load', loaded);
-			}
-		}
-	}, lazyLoadOptions);
-	observer.observe(image); // intersection observer
+  const loaded = () => {
+    image.style.opacity = '1';
+  };
+  const observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      image.src = src;
+      if (image.complete) {
+        loaded();
+      } else {
+        image.addEventListener('load', loaded);
+      }
+    }
+  }, lazyLoadOptions);
+  observer.observe(image); // intersection observer
 
-	return {
-		destroy() {
-			image.removeEventListener('load', loaded); // clean up the event listener
-		}
-	};
+  return {
+    destroy() {
+      image.removeEventListener('load', loaded); // clean up the event listener
+    }
+  };
 };
+
+export async function signInWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  const credential = await signInWithPopup(auth, provider);
+  const idToken = await credential.user.getIdToken();
+
+  await fetch('/api/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ idToken })
+  });
+}
+
+export async function signOutSSR() {
+  await fetch('/api/signout', {
+    method: 'DELETE'
+  });
+  await signOut(auth);
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import { auth } from '$lib/firebase';
-	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+  import { signInWithGoogle } from '$lib';
+  import type { PageData } from './$types';
 
-	export let data: PageData;
-
-	async function signInWithGoogle() {
-		const provider = new GoogleAuthProvider();
-		const user = await signInWithPopup(auth, provider);
-		console.log(user);
-	}
+  export let data: PageData;
 </script>
 
-<button class="btn variant-filled-primary flex items-center gap-2" on:click={signInWithGoogle}>
-	<i class="fa-brands fa-google"></i>
-	Login with Google
+<button
+  class="btn variant-filled-primary flex items-center gap-2"
+  on:click={signInWithGoogle}
+>
+  <i class="fa-brands fa-google"></i>
+  Login with Google
 </button>
