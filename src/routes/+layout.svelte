78b3b1329e6971b44fe402a3a3d9cb3831dd7114
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2023-12-23 12:43:15 +0300

    implement app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell slotSidebarLeft="w-52 bg-surface-500/10 p-4">
+	<svelte:fragment slot="header">
+		<AppBar>Header</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<div id="sidebar-left" class="hidden lg:block">Sidebar Left</div>
+	</svelte:fragment>
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+</AppShell>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { auth } from '$lib/firebase';
+	import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+	export let data: PageData;
+
+	async function signInWithGoogle() {
+		const provider = new GoogleAuthProvider();
+		const user = await signInWithPopup(auth, provider);
+		console.log(user);
+	}
+</script>
+
+<button class="btn variant-filled-primary flex items-center gap-2" on:click={signInWithGoogle}>
+	<i class="fa-brands fa-google"></i>
+	Login with Google
+</button>
