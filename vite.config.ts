d357b91d1591a4407ca2f9017199a77d74f63716
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-14 22:58:11 +0300

    remove unused dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { imagetools } from '@zerodevx/svelte-img/vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss(), imagetools(), basicSsl()]
+  plugins: [sveltekit(), purgeCss(), basicSsl()]
 });
