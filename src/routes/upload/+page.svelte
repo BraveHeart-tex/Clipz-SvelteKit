69c1629fff0cbca2a2b9497a2fdb3e4f00176a1b
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2024-01-24 17:29:33 +0300

    small fix on VideoFormHandler initialization

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -9,7 +9,6 @@
   import UploadVideoStepOne from '$lib/components/UploadVideoStepOne.svelte';
   import VideoUploadArea from '$lib/components/VideoUploadArea.svelte';
   import { VideoFormHandler } from '$/src/lib/services/video-form-handler';
-  import { onMount } from 'svelte';
   import { storage } from '$/src/lib/firebase';
   import { videoService } from '$/src/lib/services/video-service';
 
@@ -24,136 +23,125 @@
     validators: uploadVideoSchema
   });
 
-  let videoFormHandler: VideoFormHandler;
-
-  onMount(() => {
-    videoFormHandler = new VideoFormHandler({
-      modalStore,
-      toastStore,
-      superFrm,
-      isEditMode,
-      storage,
-      currentVideo: data.currentVideo || null,
-      videoService: videoService
-    });
+  let videoFormHandler = new VideoFormHandler({
+    modalStore,
+    toastStore,
+    superFrm,
+    isEditMode,
+    storage,
+    currentVideo: data.currentVideo || null,
+    videoService
   });
 </script>
 
-{#if videoFormHandler}
-  <div class="mt-2">
-    {#if $videoFormHandler.videoSrc || data.currentVideo?.url}
-      <div class="flex flex-col gap-1">
-        <h1 class="h2">
-          {isEditMode ? 'Update Your Clip' : 'Upload Your Clip'}
-        </h1>
-        <p>
-          {isEditMode ? 'Update' : 'Upload'} your video by filling out the form below
-        </p>
-      </div>
-      {#if $videoFormHandler.isSubmitting && !$videoFormHandler.submitCompleted}
-        <UploadProgress
-          title="Uploading Video"
-          description="Please do not close this window or navigate away from this page until the
+<div class="mt-2">
+  {#if $videoFormHandler.videoSrc || data.currentVideo?.url}
+    <div class="flex flex-col gap-1">
+      <h1 class="h2">
+        {isEditMode ? 'Update Your Clip' : 'Upload Your Clip'}
+      </h1>
+      <p>
+        {isEditMode ? 'Update' : 'Upload'} your video by filling out the form below
+      </p>
+    </div>
+    {#if $videoFormHandler.isSubmitting && !$videoFormHandler.submitCompleted}
+      <UploadProgress
+        title="Uploading Video"
+        description="Please do not close this window or navigate away from this page until the
       upload is complete."
-          progress={$videoFormHandler.progress}
-        />
-      {/if}
-      <div class="grid grid-cols-1 gap-4 mt-4 lg:max-w-[75%]">
-        <Form.Root
-          form={superFrm}
-          schema={uploadVideoSchema}
-          controlled
-          let:config
-          on:submit={async (e) => videoFormHandler.handleUploadSubmit(e)}
-        >
-          <div class="grid grid-cols-1 gap-2">
-            <Form.Field {config} name="title">
-              <div class="flex flex-col">
-                <Form.Label>Video Title</Form.Label>
-                <Form.Input type="text" class="input rounded-md" />
-                <Form.Validation class="text-red-500 font-semibold" />
-              </div>
-            </Form.Field>
-            <Form.Field {config} name="thumbnail">
-              <Form.Control id="thumbnail" let:attrs>
-                <div class="flex flex-col">
-                  <label for="thumbnail">Thumbnail</label>
-                  <input
-                    {...attrs}
-                    id="thumbnail"
-                    name="thumbnail"
-                    type="file"
-                    class="input rounded-md"
-                    accept="image/*"
-                    on:change={videoFormHandler.handleThumbnailChange}
-                  />
-                  <Form.Validation class="text-red-500 font-semibold" />
-                  <Form.Description>
-                    Upload a custom thumbnail for your video. If you do not
-                    upload a thumbnail, a thumbnail will be generated from your
-                    video.
-                  </Form.Description>
-                </div>
-              </Form.Control>
-            </Form.Field>
-            <Form.Field {config} name="description">
-              <div class="flex flex-col">
-                <Form.Label>Description</Form.Label>
-                <Form.Input type="text" class="input rounded-md" />
-                <Form.Validation class="text-red-500 font-semibold" />
-              </div>
-            </Form.Field>
-          </div>
-          <div class="flex items-center gap-2 w-full mt-4">
-            <button
-              type="button"
-              class="btn variant-outline-tertiary rounded-md"
-              disabled={$videoFormHandler.isSubmitting}
-              on:click={videoFormHandler.handleCancelUploadClick}>Cancel</button
-            >
-            <button
-              type="submit"
-              disabled={$videoFormHandler.isSubmitting ||
-                (!$videoFormHandler.videoSrc && !data.currentVideo?.url)}
-              class="btn variant-filled-primary rounded-md"
-            >
-              Upload
-            </button>
-          </div>
-        </Form.Root>
-
-        {#if isEditMode}
-          <p>
-            If you would like to upload a different video, you can drop your
-            video to the zone below or click on it to select a file.
-          </p>
-          <VideoUploadArea
-            onChangeHandler={videoFormHandler.handleVideoChange}
-          />
-        {/if}
-      </div>
-
-      {#if $videoFormHandler.videoSrc}
-        <UploadVideoPreview
-          handleRemoveThumbnail={videoFormHandler.handleRemoveThumbnail}
-          videoSrc={$videoFormHandler.videoSrc}
-          poster={$videoFormHandler.poster ||
-            data?.currentVideo?.poster_url ||
-            ''}
-        />
-      {:else if data.currentVideo?.url}
-        <UploadVideoPreview
-          handleRemoveThumbnail={videoFormHandler.handleRemoveThumbnail}
-          videoSrc={data?.currentVideo?.url}
-          poster={$videoFormHandler.poster ||
-            data?.currentVideo?.poster_url ||
-            ''}
-        />
-      {/if}
-    {:else}
-      <UploadVideoStepOne
-        onChangeHandler={videoFormHandler.handleVideoChange}
+        progress={$videoFormHandler.progress}
       />
     {/if}
-  </div>
-{/if}
+    <div class="grid grid-cols-1 gap-4 mt-4 lg:max-w-[75%]">
+      <Form.Root
+        form={superFrm}
+        schema={uploadVideoSchema}
+        controlled
+        let:config
+        on:submit={async (e) => videoFormHandler.handleUploadSubmit(e)}
+      >
+        <div class="grid grid-cols-1 gap-2">
+          <Form.Field {config} name="title">
+            <div class="flex flex-col">
+              <Form.Label>Video Title</Form.Label>
+              <Form.Input type="text" class="input rounded-md" />
+              <Form.Validation class="text-red-500 font-semibold" />
+            </div>
+          </Form.Field>
+          <Form.Field {config} name="thumbnail">
+            <Form.Control id="thumbnail" let:attrs>
+              <div class="flex flex-col">
+                <label for="thumbnail">Thumbnail</label>
+                <input
+                  {...attrs}
+                  id="thumbnail"
+                  name="thumbnail"
+                  type="file"
+                  class="input rounded-md"
+                  accept="image/*"
+                  on:change={videoFormHandler.handleThumbnailChange}
+                />
+                <Form.Validation class="text-red-500 font-semibold" />
+                <Form.Description>
+                  Upload a custom thumbnail for your video. If you do not upload
+                  a thumbnail, a thumbnail will be generated from your video.
+                </Form.Description>
+              </div>
+            </Form.Control>
+          </Form.Field>
+          <Form.Field {config} name="description">
+            <div class="flex flex-col">
+              <Form.Label>Description</Form.Label>
+              <Form.Input type="text" class="input rounded-md" />
+              <Form.Validation class="text-red-500 font-semibold" />
+            </div>
+          </Form.Field>
+        </div>
+        <div class="flex items-center gap-2 w-full mt-4">
+          <button
+            type="button"
+            class="btn variant-outline-tertiary rounded-md"
+            disabled={$videoFormHandler.isSubmitting}
+            on:click={videoFormHandler.handleCancelUploadClick}>Cancel</button
+          >
+          <button
+            type="submit"
+            disabled={$videoFormHandler.isSubmitting ||
+              (!$videoFormHandler.videoSrc && !data.currentVideo?.url)}
+            class="btn variant-filled-primary rounded-md"
+          >
+            Upload
+          </button>
+        </div>
+      </Form.Root>
+
+      {#if isEditMode}
+        <p>
+          If you would like to upload a different video, you can drop your video
+          to the zone below or click on it to select a file.
+        </p>
+        <VideoUploadArea onChangeHandler={videoFormHandler.handleVideoChange} />
+      {/if}
+    </div>
+
+    {#if $videoFormHandler.videoSrc}
+      <UploadVideoPreview
+        handleRemoveThumbnail={videoFormHandler.handleRemoveThumbnail}
+        videoSrc={$videoFormHandler.videoSrc}
+        poster={$videoFormHandler.poster ||
+          data?.currentVideo?.poster_url ||
+          ''}
+      />
+    {:else if data.currentVideo?.url}
+      <UploadVideoPreview
+        handleRemoveThumbnail={videoFormHandler.handleRemoveThumbnail}
+        videoSrc={data?.currentVideo?.url}
+        poster={$videoFormHandler.poster ||
+          data?.currentVideo?.poster_url ||
+          ''}
+      />
+    {/if}
+  {:else}
+    <UploadVideoStepOne onChangeHandler={videoFormHandler.handleVideoChange} />
+  {/if}
+</div>
